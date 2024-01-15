af3b4528c731085dfacb6cbe20fb1b27877bd862
404 <schy2504@naver.com>
2024-01-15 20:30:27 +0900

    beakjoon q2577 #2

---
+++ java_baek/q2577.java
@@ -0,0 +1,24 @@
+package java_baek;
+
+import java.util.Scanner;
+
+public class q2577 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] list = new int[10];
+        int addUp = 1;
+
+        for(int i = 0; i < 3; i++){
+            addUp *= scanner.nextInt();
+        }
+
+        while (addUp != 0){
+            list[addUp%10]++;
+            addUp /= 10;
+        }
+
+        for(int i = 0; i < 10; i++){
+            System.out.println(list[i]);
+        }
+    }
+}
