7cda0491f9178f6a5bf8e9bdde388ee1fa3884a1
404 <schy2504@naver.com>
2024-01-15 20:35:16 +0900

    beakjoon q2577 #2
    fixes #2

--- java_baek/q2577.java
+++ java_baek/q2577.java
@@ -2,6 +2,7 @@ package java_baek;
 
 import java.util.Scanner;
 
+// 너무 쉬웠어요. 유치원 수준?
 public class q2577 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
