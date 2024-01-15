e8f2c09c3ee50524ab34398b8915137f0d1bcce6
404 <schy2504@naver.com>
2024-01-15 19:15:32 +0900

    adding code

--- java_baek/test.java
+++ java_baek/test.java
@@ -1,4 +1,4 @@
-package java_beak;
+package java_baek;
 
 public class test {
     public static void main(String[] args) {
--- java_baek/test2.java
+++ java_baek/test2.java
@@ -1,4 +1,4 @@
-package java_beak;
+package java_baek;
 
 public class test2 {
     public static void main(String[] args) {
